f42b71f7d2a20c0195ebbe26e29ba2732168de8c
Ken Yoneda <ken.yoneda92@gmail.com>
2020-04-30 18:51:11 -0400

    Update endpoint from datamine.mta.info to api.mta.info

--- gtfs_rt_fetcher.py
+++ gtfs_rt_fetcher.py
@@ -1,34 +1,37 @@
-'''
+"""
 Script to continuously download MTA GTFS realtime data and save to disk
-'''
+"""
 import datetime
 import os
 import requests
 import time
 
 API_KEY = "API_KEY"
-BASE_URL = "http://datamine.mta.info/mta_esi.php?key={}&feed_id={}"
-FEED_IDS = {'123456S': '1',
-            'ACEHS': '26',
-            'NQRW': '16',
-            'BDFM': '21',
-            'L': '2',
-            'SIR': '11',
-            'G': '31',
-            'JZ': '36',
-            '7': '51'}
+BASE_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/{}"
+HEADERS = {"x-api-key": API_KEY}
+FEED_IDS = {"1": "nyct%2Fgtfs",
+            "26": "nyct%2Fgtfs-ace",
+            "16": "nyct%2Fgtfs-nqrw",
+            "21": "nyct%2Fgtfs-bdfm",
+            "2": "nyct%2Fgtfs-l",
+            "11": "nyct%2Fgtfs-si",
+            "31": "nyct%2Fgtfs-g",
+            "36": "nyct%2Fgtfs-jz",
+            "51": "nyct%2Fgtfs-7",
+            "-lirr": "lirr%2Fgtfs-lirr",
+            "-mnr": "mnr%2Fgtfs-mnr"}
 FEED_INTERVAL_SEC = 30
-ROOT_DIR = 'data'
+ROOT_DIR = "data"
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H-%M-%S"
 FILENAME = "feed{}-{}-{}"  # feed no, date, time
 
 
 def get_gtfs_rt_data():
-    '''Downloads GTFS real-time feed for every line'''
-    for lines, feed in FEED_IDS.items():
+    """Downloads GTFS real-time feed for every line"""
+    for feed, url_suffix in FEED_IDS.items():
         # Generate path to write files
-        the_feed = 'feed' + feed
+        the_feed = "feed" + feed
         d = datetime.datetime.utcnow()
         year = str(d.year)
         month = str(d.month).zfill(2)
@@ -38,7 +41,7 @@ def get_gtfs_rt_data():
 
         # Download .proto file
         try:
-            r = requests.get(BASE_URL.format(API_KEY, feed))
+            r = requests.get(BASE_URL.format(url_suffix), headers=HEADERS)
         except requests.exceptions.RequestException:
             continue
 
@@ -51,13 +54,14 @@ def get_gtfs_rt_data():
             the_time = d.strftime(TIME_FORMAT)
             filename = FILENAME.format(feed, the_date, the_time)
             full_path = path + os.path.sep + filename
-            with open(full_path, 'wb') as f:
+
+            with open(full_path, "wb") as f:
                 f.write(r.content)
 
 
 def run_every(sec, func):
-    '''Helper to run function every n seconds with
-    best attempt to account for drift'''
+    """Helper to run function every n seconds with
+    best attempt to account for drift"""
     def tick():
         start = time.time()
         count = 0
@@ -75,5 +79,5 @@ def run_every(sec, func):
         func()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_every(FEED_INTERVAL_SEC, get_gtfs_rt_data)
